bf19c73345cae3fb260719b2577e67de3b00a562
huachaoZheng <kissnana5@live.cn>
2018-01-24 23:16:02 +0800

    second commit

---
+++ MicroTask.IdentityAuthServer/Config.cs
@@ -0,0 +1,40 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroTask.IdentityAuthServer
+{
+
+    public class Config
+    {
+         
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId="client",
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    ClientSecrets=
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes ={"api1" }
+                 }
+           };
+        }
+
+    }
+
+}
---
+++ MicroTask.Task/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Steeltoe.Discovery.Client;
+
+namespace MicroTask.Task.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly DiscoveryHttpClientHandler _handler;
+        private const string WORK_JOURNAL_URLS = "http://WorkJournal/api/values";
+
+        public ValuesController(IDiscoveryClient client)
+        {
+            this._handler = new DiscoveryHttpClientHandler(client);
+
+        }
+
+        // GET api/values
+        [HttpGet("WorkJournal")]
+        public async Task<string> Get()
+        {
+            var client = new HttpClient(_handler,false);
+
+            return await client.GetStringAsync(WORK_JOURNAL_URLS);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

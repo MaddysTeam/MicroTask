5bc812077b1a903da82f11f6ac4333f41c6252d9
huachaoZheng <kissnana5@live.cn>
2018-01-24 23:09:55 +0800

    first commit

---
+++ MicroTask.Project/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+
+namespace MicroTask.Project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                  .UseCloudFoundryHosting()
+                  .AddCloudFoundry()
+                  .UseStartup<Startup>()
+                  .UseUrls("http://*:8050")
+                  .Build();
+    }
+}
